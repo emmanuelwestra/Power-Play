78504284f0939cb8d3313882cf358da05137c181
emmanuelwestra <ewestra@ualr.edu>
2023-09-01 19:32:54 -0500

    Declarations, MonsterAttack
    I finished the declarations for the new functions, the function prototypes, and completed the MonsterAttack() function.

--- PowerPlay_2.cpp
+++ PowerPlay_2.cpp
@@ -11,8 +11,14 @@ struct Object {
     int health;
 };
 
-// definitions
+// function prototypes
 std::vector<Object> loadMonsters();
+void displayBattle(Object player, std::vector<Object> monsters);
+void monsterAttack(Object player, std::vector<Object> monsters);
+void playerAttack(Object player, std::vector<Object> monsters);
+void attack(Object player, std::vector<Object> monsters);
+void defend(Object player, std::vector<Object> monsters);
+void heal(Object player, std::vector<Object> monsters);
 
 // global
 std::random_device seed;
@@ -24,8 +30,17 @@ int main() {
     std:: cin >> player.name;
     std::vector<Object> monsters = loadMonsters();
     while(player.health > 0 && monsters.size() > 0) {
-        displayBattle();
-        std::cin >> fish;
+        displayBattle(player, monsters);
+		char command{' '};
+        while(command!= 'a' && command!='h') {
+            std::cout << "What do you do? (a)ttack (h)eal ";
+            std::cin >> command;
+        }
+		if(command == 'a')
+            monsterAttack(player, monsters);
+        else
+            defend();
+        attack();
     }
     return 0;
 }
@@ -47,23 +62,43 @@ std::vector<Object> loadMonsters() {
 }
 
 // Almost a copy and paste from PP1
-void displayBattle() {
-
+void displayBattle(Object player, std::vector<Object> monsters) {
+    std::cout << player.name << ": " << player.health << std::endl;
+    std::cout << "Monsters: " << std::endl;
+    for (int i{ 0 }; i < monsters.size(); i++){
+        std::cout << i + 1 << ". " << monsters[i].name << ": " << monsters[i].health << std::endl;
+    }
 }
 
 // Almost a copy and paste from PP1
-// monsterAttack();
+void monsterAttack(Object player, std::vector<Object> monsters) {
+    std::cout << "Which Monster: ";
+    int monsterNum{ 0 };
+    std::cin >> monsterNum;
+    if (monsterNum > 0 && monsterNum <= monsters.size())
+    {
+        monsters[monsterNum - 1].health -= player.strength;
+    }
+}
 
 // // Almost a copy and paste from PP1
-// playerAttack();
+void playerAttack(Object player, std::vector<Object> monsters) {
+
+}
 
 // // returns damage done using normal distribution and the passed in object’s strength. I also output the object’s name, along with “ deals “ mean: attacker.strength, stdev: 2.0
-// attack();
+void attack(Object player, std::vector<Object> monsters) {
+
+}
 
 // // takes the defending object and the damage being done. subtracts the damage from the defender’s health. outputs: damage << " damage to " << object.name << "!!!" << std::endl;
 // // The code for a fight looks like this for the player attacking a monster:
 //  defend(monsters[monsterNum - 1], attack(player));
-// defend();
+void defend(Object player, std::vector<Object> monsters) {
+
+}
 
 // // uses a normal distribution with median of strength*2 and standard deviation of 3. adds random amount to object’s health.
-// heal();
+void heal(Object player, std::vector<Object> monsters) {
+
+}
